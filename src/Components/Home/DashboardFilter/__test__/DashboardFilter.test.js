dba19a3dff6fa068777753fb069baca2ecb6b9a4
lucasnerytoledo <lucas.leite@petlove.com.br>
2022-09-06 10:38:04 -0300

    mockando setDateFilter e inserindo o lint no test de filter

--- src/Components/Home/DashboardFilter/__test__/DashboardFilter.test.js
+++ src/Components/Home/DashboardFilter/__test__/DashboardFilter.test.js
@@ -16,21 +16,22 @@ const months = [
     'outubro',
     'novembro',
     'dezembro'
-  ]
-  const years = ['2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030']
-  
+]
+const years = ['2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030']
+
+const setDateFilter = jest.fn()
 
 describe('Testing render component', () => {
     it('render correctly and show select', () => {
-        const { queryByTestId } = render(<DashboardFilter setDateFilter={() => {}} monthToday={'setembro'} yearToday={2022} months={months} years={years}/>)
-        
+        const { queryByTestId } = render(<DashboardFilter setDateFilter={setDateFilter} monthToday={'setembro'} yearToday={2022} months={months} years={years}/>)
+
         expect(queryByTestId('selectMonth')).toBeTruthy()
         expect(queryByTestId('selectYear')).toBeTruthy()
     })
 
     it('render with default value equal setembro for month and 2022 for year', () => {
-        const { container: wrapper } = render(<DashboardFilter setDateFilter={() => {}} monthToday={'setembro'} yearToday={2022} months={months} years={years}/>)
-        
+        const { container: wrapper } = render(<DashboardFilter setDateFilter={setDateFilter} monthToday={'setembro'} yearToday={2022} months={months} years={years}/>)
+
         const monthElement = wrapper.getElementsByTagName('select')[0]
         const yearElement = wrapper.getElementsByTagName('select')[1]
 
@@ -40,12 +41,12 @@ describe('Testing render component', () => {
 })
 
 describe('Testing selects value', () => {
-    let container 
+    let container
 
     it('updates on change', () => {
-        const { container: wrapper } = render(<DashboardFilter setDateFilter={() => {}} monthToday={'setembro'} yearToday={2022} months={months} years={years}/>)
+        const { container: wrapper } = render(<DashboardFilter setDateFilter={setDateFilter} monthToday={'setembro'} yearToday={2022} months={months} years={years}/>)
         container = wrapper
-        
+
         const monthElement = container.getElementsByTagName('select')[0]
         const yearElement = container.getElementsByTagName('select')[1]
         fireEvent.change(monthElement, { target: { value: 'outubro' }})
